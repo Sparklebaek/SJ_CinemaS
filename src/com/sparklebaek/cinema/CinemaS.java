819719c5e6d1ab11da072a60956340e3d4c671f4
Baek SeungJae <sparklebaek@naver.com>
2021-05-21 17:31:52 +0900

    SJ_CinemaS Opening commit

---
+++ src/com/sparklebaek/cinema/CinemaS.java
@@ -0,0 +1,11 @@
+package com.sparklebaek.cinema;
+
+public class CinemaS {
+
+	public static void main(String[] args) {
+		MovieName movie = MovieName.배트맨;
+		System.out.println("SJ 시네마에 오신것을 환영합니다");
+		System.out.println(movie);
+	}
+
+}
---
+++ src/com/sparklebaek/cinema/MovieName.java
@@ -0,0 +1,5 @@
+package com.sparklebaek.cinema;
+
+public enum MovieName {
+    슈퍼맨, 배트맨, 아쿠아맨, 그린랜턴
+}
