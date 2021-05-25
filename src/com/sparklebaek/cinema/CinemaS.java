a7e65060e7dba5d84cd67dcb6ebcdcdd56248001
Baek SeungJae <sparklebaek@naver.com>
2021-05-25 17:55:34 +0900

    영화 종류 문자열 출력 기능 추가

--- src/com/sparklebaek/cinema/CinemaS.java
+++ src/com/sparklebaek/cinema/CinemaS.java
@@ -1,11 +1,26 @@
 package com.sparklebaek.cinema;
 
+import java.util.Arrays;
+
 public class CinemaS {
 
 	public static void main(String[] args) {
 		MovieName movie = MovieName.배트맨;
 		System.out.println("SJ 시네마에 오신것을 환영합니다");
-		System.out.println(movie);
+		MovieName movieNames[] = MovieName.values();
+		
+		/**
+		 * 나중에 Enum to List 사용하여 깔금하게 정리
+		 */
+		String menu = String.join("/", 
+				MovieName.그린랜턴.toString(),
+				MovieName.배트맨.toString(),
+				MovieName.슈퍼맨.toString(),
+				MovieName.아쿠아맨.toString() );
+		
+		System.out.println(menu);
+		String[] menus = menu.split("/");
+		return ;
 	}
 
 }
