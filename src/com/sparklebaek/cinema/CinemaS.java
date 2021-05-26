6c1b90a99d7da36fcd22939dd1c17dc248e6dbd8
Baek SeungJae <sparklebaek@naver.com>
2021-05-26 11:09:03 +0900

    Scanner 관련 명령어 추가

--- src/com/sparklebaek/cinema/CinemaS.java
+++ src/com/sparklebaek/cinema/CinemaS.java
@@ -1,10 +1,14 @@
 package com.sparklebaek.cinema;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CinemaS {
 
 	public static void main(String[] args) {
+		/**
+		 * stringbuilder ==> stringbuffer로
+		 */
 		MovieName movie = MovieName.배트맨;
 		System.out.println("SJ 시네마에 오신것을 환영합니다");
 		MovieName movieNames[] = MovieName.values();
@@ -19,6 +23,12 @@ public class CinemaS {
 				MovieName.아쿠아맨.toString() );
 		
 		System.out.println(menu);
+		System.out.println("시청할 영화 입력: ");
+		Scanner scanner = new Scanner(System.in);
+		int 영화이름 = scanner.nextInt();
+		System.out.println("영화 " + 영화이름 + "를 선택하셨습니다.");
+		scanner.close();
+		
 		String[] menus = menu.split("/");
 		return ;
 	}
